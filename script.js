6aa74f948834738f1f0920d7ce263948315d2ff7
kaleyfawcett <kaley.fawcett@gmail.com>
2020-05-01 23:14:05 -0400

    first step

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log('Add validation!');
+let form = document.querySelector('#parking-form')
+let formIsValid
+
+form.addEventListener('submit', function (event) {
+    // formIsValid = true
+    event.preventDefault()
+
+    // removeErrorMessage()
+
+
+    // validateName ()
+    // validateCarYear ()
+    // validateCarMake ()
+    // validateCarModel ()
+    // validateDateParking ()
+    // validateNumberOfDays ()
+    // validateCreditCard ()
+    // validateCVV ()
+    // validateExpiration ()
+
+
+// function ValidateName () {
+    let nameInput = document.querySelector('#name');
+    let name = nameInput.value;
+
+
+
+    if (name) {
+        let nameValid = document.getElementById("name-field")
+        nameValid.classList.remove ('input-invalid')
+        nameValid.classList.add ('input-valid')
+    } else {
+        let nameValid = document.getElementById("name-field")
+        nameValid.classList.add('input-invalid')
+        nameValid.classList.remove ('input-valid')
+        
+    }
+
+})
