d6626ea426694658138f2cfe2cdc4089c60d28ef
kaleyfawcett <kaley.fawcett@gmail.com>
2020-05-02 21:48:26 -0400

    step 2

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ let formIsValid
 
 // Step 1
 
+//function validate name
 form.addEventListener('submit', function (event) {
     event.preventDefault()
 
-    
-// function ValidateName () {
 let nameInput = document.querySelector('#name');
 let name = nameInput.value;
-
-
 
     if (name) {
         let nameValid = document.getElementById("name-field")
@@ -21,13 +18,14 @@ let name = nameInput.value;
         let nameValid = document.getElementById("name-field")
         nameValid.classList.add('input-invalid')
         nameValid.classList.remove ('input-valid')
+        nameError.innerHTML = 'Name is required'
         
-    }}) 
+}}) 
 
+//function validate car year & make & model
 form.addEventListener('submit', function (event) {
     event.preventDefault()
 
-//function ValidateCarYear & make & model
 let carYearInput = document.querySelector('#car-year');
 let carYear = carYearInput.value;
 
@@ -47,10 +45,9 @@ let carModel = carModelInput.value;
         let carStatus = document.getElementById("car-field")
         carStatus.classList.add('input-invalid')
         carStatus.classList.remove ('input-valid')
+        carError.innerHTML = "Car Information is required"
 
-    }    
-
-})
+}})
 
 // fuction validate date parking 
 form.addEventListener('submit', function (event) {
@@ -68,9 +65,9 @@ if (startDate) {
     let startDateStatus = document.getElementById ('start-date-field')
     startDateStatus.classList.add ('input-invalid')
     startDateStatus.classList.remove ('input-valid')  
+    dateError.innerHTML = 'Date is required'
 
-}
-})
+}})
 
 //function validate number of days 
 form.addEventListener('submit', function (event) {
@@ -88,9 +85,9 @@ if (days) {
     let dayStatus = document.getElementById ('days-field')
     dayStatus.classList.add ('input-invalid')
     dayStatus.classList.remove ('input-valid')  
+    dayError.innerHTML = 'Number of days is required'
 
-}
-})
+}})
 
 //function validate credit card
 form.addEventListener('submit', function (event) {
@@ -107,10 +104,10 @@ if (creditCard) {
 } else {
     let creditCardInput = document.getElementById ('credit-card-field')
     creditCardInput.classList.add ('input-invalid')
-    creditCardInput.classList.remove ('input-valid')  
-
-}
-})
+    creditCardInput.classList.remove ('input-valid')
+    creditError.innerHTML = 'Credit card is required'
+      
+}})
 
 //function validate cvv
 form.addEventListener('submit', function (event) {
@@ -128,9 +125,9 @@ if (cvv) {
     let cvvInput = document.getElementById ('cvv-field')
     cvvInput.classList.add ('input-invalid')
     cvvInput.classList.remove ('input-valid')  
+    cvvError.innerHTML = 'CVV is required'
 
-}
-})
+}})
 
 //function validate expiration date 
 form.addEventListener('submit', function (event) {
@@ -148,6 +145,60 @@ if (expiration) {
     let expirationInput = document.getElementById ('expiration-field')
     expirationInput.classList.add ('input-invalid')
     expiration.classList.remove ('input-valid')  
+    expirationError.innerHTML = 'Expiration is required'
 
-}
-})
+}})
+
+//step 2 
+
+document.querySelector ('#name-field')
+let nameError = document.createElement('div')
+let errorDiv = document.querySelector ("#name-field")
+errorDiv.appendChild (nameError) 
+nameError.className = ('nameError')
+
+
+document.querySelector ('#car-field')
+let carError = document.createElement('div')
+let carDiv = document.querySelector ('#car-field')
+carDiv.appendChild (carError) 
+carError.className = ('carError')
+
+
+document.querySelector ('#start-date-field')
+let dateError = document.createElement ('div')
+let dateErrorDiv = document.querySelector ('#start-date-field')
+dateErrorDiv.appendChild (dateError)
+dateError.className = ('dateError')
+
+
+document.querySelector ('#days-field')
+let dayError = document.createElement('div')
+let dayErrorDiv = document.querySelector ("#days-field")
+dayErrorDiv.appendChild (dayError) 
+dayError.className = ('dayError')
+
+
+document.querySelector ('#credit-card-field')
+let creditError = document.createElement('div')
+let creditErrorDiv = document.querySelector ("#credit-card-field")
+creditErrorDiv.appendChild (creditError) 
+creditError.className = ('creditError')
+
+
+document.querySelector ('#cvv-field')
+let cvvError = document.createElement('div')
+let cvvErrorDiv = document.querySelector ("#cvv-field")
+cvvErrorDiv.appendChild (cvvError) 
+cvvError.className = ('cvvError') 
+
+
+document.querySelector ('#expiration-field')
+let expirationError = document.createElement('div')
+let expirationErrorDiv = document.querySelector ("#expiration-field")
+expirationErrorDiv.appendChild (expirationError) 
+expirationError.className = ('expirationError')  //error needs to be found here 
+
+//step 3
+
+
